896e25c549967be9612ae166977483ad4d04e521
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-11-24 21:51:18 -0500

    Add cardToInPlayCard

--- StarRealms/Game/InPlayCard.hs
+++ StarRealms/Game/InPlayCard.hs
@@ -1,5 +1,6 @@
 module StarRealms.Game.InPlayCard where
 
+import StarRealms.Card
 import StarRealms.Card.Ability
 import StarRealms.Card.Base
 import StarRealms.Card.Faction
@@ -15,15 +16,15 @@ data InPlayCard
 data InPlayBase
   = InPlayBase
   { original :: Base
-  , name    :: Text
-  , faction :: [Faction]
-  , cost    :: Natural
-  , defense :: Natural
-  , damage  :: Natural
-  , outpost :: Bool
-  , primary :: Maybe InPlayAbility
-  , ally    :: Maybe InPlayAbility
-  , scrap   :: Maybe InPlayAbility
+  , name     :: Text
+  , faction  :: [Faction]
+  , cost     :: Natural
+  , defense  :: Natural
+  , damage   :: Natural
+  , outpost  :: Bool
+  , primary  :: Maybe InPlayAbility
+  , ally     :: Maybe InPlayAbility
+  , scrap    :: Maybe InPlayAbility
   }
 
 data InPlayShip
@@ -40,3 +41,37 @@ data InPlayShip
 data InPlayAbility
   = Played Ability
   | Unplayed Ability
+
+-- Derive a default InPlayCard from a Card.
+cardToInPlayCard :: Card -> InPlayCard
+cardToInPlayCard = \case
+  CardBase card -> InPlayCardBase (baseToInPlayBase card)
+  CardShip card -> InPlayCardShip (shipToInPlayShip card)
+
+  where
+    baseToInPlayBase :: Base -> InPlayBase
+    baseToInPlayBase base =
+      InPlayBase
+        { original = base
+        , name     = view (the @"name") base
+        , faction  = [view (the @"faction") base]
+        , cost     = view (the @"cost") base
+        , defense  = view (the @"defense") base
+        , damage   = 0
+        , outpost  = view (the @"outpost") base
+        , primary  = Unplayed <$> view (the @"primary") base
+        , ally     = Unplayed <$> view (the @"ally") base
+        , scrap    = Unplayed <$> view (the @"scrap") base
+        }
+
+    shipToInPlayShip :: Ship -> InPlayShip
+    shipToInPlayShip ship =
+      InPlayShip
+        { original = ship
+        , name     = view (the @"name") ship
+        , faction  = maybe [] pure (view (the @"faction") ship)
+        , cost     = view (the @"cost") ship
+        , primary  = view (the @"primary") ship
+        , ally     = Unplayed <$> view (the @"ally") ship
+        , scrap    = Unplayed <$> view (the @"scrap") ship
+        }
