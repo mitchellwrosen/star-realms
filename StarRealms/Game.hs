f82036d19c3fe139c84a8cda1d6d55e200546086
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-11-25 21:52:36 -0500

    Combine Game/GameState

--- StarRealms/Game.hs
+++ StarRealms/Game.hs
@@ -3,60 +3,65 @@ module StarRealms.Game where
 import StarRealms.Card
 import StarRealms.Player
 
-import qualified StarRealms.Deck as Deck
-import StarRealms.Game.Action
-import StarRealms.Card.Choice
+import           StarRealms.Card.Choice
+import qualified StarRealms.Deck        as Deck
+import           StarRealms.Game.Action
 
+import Alg.Functor
 import Mitchell.Prelude
 import Optic.Fold
-import Alg.Functor
 
 data Game
   = Game
-  { player1      :: Player
-  , player2      :: Player
-  , tradeDeck    :: [Card]
-  , scrapHeap    :: [Card]
-  , tradeRow     :: [Card]
+  { state     :: GameState
+  , player    :: WhichPlayer
+  , player1   :: Player
+  , player2   :: Player
+  , tradeDeck :: [Card]
+  , scrapHeap :: [Card]
+  , tradeRow  :: [Card]
   } deriving stock (Generic)
 
-gamePlayer :: WhichPlayer -> Lens' Game Player
-gamePlayer = \case
-  Player1 -> the @"player1"
-  Player2 -> the @"player2"
+-- | Get the current player of a game.
+getCurrentPlayer :: Game -> Player
+getCurrentPlayer game =
+  case game ^. the @WhichPlayer of
+    Player1 -> game ^. the @"player1"
+    Player2 -> game ^. the @"player2"
 
+-- | The state of the game, which determines which messages are valid.
 data GameState
-  = GameStateMain WhichPlayer
-  | GameStateDiscard WhichPlayer
-  | GameStateScrap WhichPlayer
-  | GameStateMayDestroyBase WhichPlayer
+  -- | The "main" state.
+  = GameStateMain
+  -- | The "discard" state. We are waiting for the opponent to discard a card.
+  | GameStateDiscard
+  -- | The "scrap" state. We are waiting for the current player to decide what
+  -- card to discard from their hand.
+  | GameStateScrap
+  -- | The "may destroy base" state. We are waiting for the current player to
+  -- decide if they want to destroy a base.
+  | GameStateMayDestroyBase
   | GameStateDone
 
 data UpdateGameResult
   = ActionInvalid
-  | ActionSuccessful GameState Game
+  | ActionSuccessful Game
 
-updateGame :: WhichPlayer -> Action -> GameState -> Game -> UpdateGameResult
-updateGame player action state game =
-  case state of
-    GameStateMain player' ->
-      if player == player'
+updateGame :: WhichPlayer -> Action -> Game -> UpdateGameResult
+updateGame player action game =
+  case game ^. the @"state" of
+    GameStateMain ->
+      if player == game ^. the @WhichPlayer
         then case action of
           ActionPlayCardFromHand card choice ->
-            let
-              hand :: [Text]
-              hand = toListOf
-                ( gamePlayer player
-                . the @"hand"
-                . folded
-                . cardName ) game
-            in case pluckCardFromHand card playerP of
+            case pluckCardFromHand card (getCurrentPlayer game) of
               Nothing -> ActionInvalid
               Just (card', player'') ->
                 case cardPrimaryAbility (getCardByName card) of
-                  Nothing -> if choice == ChoiceNone
-                    then ActionSuccessful state undefined
-                    else ActionInvalid
+                  Nothing ->
+                    if choice == ChoiceNone
+                      then ActionSuccessful undefined
+                      else ActionInvalid
                   Just ability -> undefined
 
           ActionUseAbilityOnCard card typ choice -> undefined
@@ -67,9 +72,6 @@ updateGame player action state game =
           ActionScrap card -> ActionInvalid
           ActionEndTurn -> undefined
         else ActionInvalid
-  where
-    playerP :: Player
-    playerP = view (gamePlayer player) game
 
 -- abilityHasChoice :: Ability -> Choice -> something?
 abilityHasChoice = undefined
